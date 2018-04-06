c72897411c3c71649b82afc333a967679cb391ba
Ryan Tsang <rvtsang@gmail.com>
2018-04-06 11:10:43 -0700

    enclosed main part of program into a for loop that cycles through all the images in the img folder

--- main.py
+++ main.py
@@ -41,119 +41,124 @@ for subdir, dirs, files in os.walk('./img/'):
 cvt = CVTools()
 displayPanel = Display()
 
-# HACK USER CHANGED VARS
+# HACK USER CHANGED VARS xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 # backgroundSubK must be odd
 backgroundSubK = 255
 closeK = 25
 threshVal = 25
 
-# load images to vars here. 0 is the gray mask
-# test (invasive cell), 6 (complicated), 3 (perfect)
-# 10.tif is the confocal
-# TODO fix image 8. seems to not work well with this
-img_main = cv.imread('img/10.tif')
-img_main = cv.cvtColor(img_main, cv.COLOR_RGB2GRAY)
-orig = img_main.copy()
-# original image to draw on
-origDraw = img_main.copy()
+# TODO make a loop function to run through all the files.
 
-'''
-////////////////////////////////////////////////////////////////////////////
-// Image Processing 
-////////////////////////////////////////////////////////////////////////////
-'''
-# blur image
-dst = cvt.BackSub(img_main, backgroundSubK)
+for imageNumber in range (len(imgFileNames)):
 
-# close image
-dst1 = cvt.Close(dst, closeK)
+    # load images to vars here. 0 is the gray mask
+    # test (invasive cell), 6 (complicated), 3 (perfect)
+    # 10.tif is the confocal
+    # TODO fix image 8. seems to not work well with this
+    imgFilePath = "img/" + imgFileNames[imageNumber]
+    img_main = cv.imread(imgFilePath)
+    img_main = cv.cvtColor(img_main, cv.COLOR_RGB2GRAY)
+    orig = img_main.copy()
+    # original image to draw on
+    origDraw = img_main.copy()
 
-# global threshold
-dst2 = cvt.Thresh(dst1, threshVal)
+    '''
+    ////////////////////////////////////////////////////////////////////////////
+    // Image Processing 
+    ////////////////////////////////////////////////////////////////////////////
+    '''
+    # blur image
+    dst = cvt.BackSub(img_main, backgroundSubK)
 
-# test close
-dst2 = cvt.Close(dst2, 50)
+    # close image
+    dst1 = cvt.Close(dst, closeK)
 
-# floodfill
-dst3 = cvt.ImFill(dst2)
+    # global threshold
+    dst2 = cvt.Thresh(dst1, threshVal)
 
+    # test close
+    dst2 = cvt.Close(dst2, 50)
 
-
-'''
-////////////////////////////////////////////////////////////////////////////
-// Data Collection and Drawing
-////////////////////////////////////////////////////////////////////////////
-'''
-
-# Data to collect
-'''
-* contour data ? this already gets collected though
-* light data at the specified points... make sure you handle background at some point
-'''
-
-# get contours, largest contour index, and the largest contour centroid
-imgContours = cvt.GetContours(dst3)
-largestContourIndex = cvt.GetLargestContour(imgContours)
-mainBodyCentroid = cvt.GetCentroid(imgContours[largestContourIndex])
-
-# get all other centroids, store in an array offBodyCentroids
-offBodyContourIndeces = cvt.GetNonMainbodyContours(imgContours, largestContourIndex)
-offBodyCentroids = []
-for i in range(len(offBodyContourIndeces)):
-    c = cvt.GetCentroid(imgContours[offBodyContourIndeces[i]])
-    offBodyCentroids.append(c)
-
-# get the distance between the offbodies and the central centroid. Store in an array
-distancesFromCentroid = []
-
-for i in range(len(offBodyCentroids)):
-    print ("offpoint: " + str(offBodyCentroids[i]))
-    print("central point: " + str(mainBodyCentroid))
-    d = cvt.GetDistanceBetween(mainBodyCentroid, offBodyCentroids[i])
-    print(d)
-
-    # add to array
-    distancesFromCentroid.append(d)
-
-    # visualize the offbody connection to the central location
-    cvt.DrawOffBodyConnections(mainBodyCentroid, offBodyCentroids[i], origDraw)
-
-
-# HACK get average dist from centroid
-# HACK get max dist from centroid
+    # floodfill
+    dst3 = cvt.ImFill(dst2)
 
 
 
+    '''
+    ////////////////////////////////////////////////////////////////////////////
+    // Data Collection and Drawing
+    ////////////////////////////////////////////////////////////////////////////
+    '''
+
+    # Data to collect
+    '''
+    * contour data ? this already gets collected though
+    * light data at the specified points... make sure you handle background at some point
+    '''
+
+    # get contours, largest contour index, and the largest contour centroid
+    imgContours = cvt.GetContours(dst3)
+    largestContourIndex = cvt.GetLargestContour(imgContours)
+    mainBodyCentroid = cvt.GetCentroid(imgContours[largestContourIndex])
+
+    # get all other centroids, store in an array offBodyCentroids
+    offBodyContourIndeces = cvt.GetNonMainbodyContours(imgContours, largestContourIndex)
+    offBodyCentroids = []
+    for i in range(len(offBodyContourIndeces)):
+        c = cvt.GetCentroid(imgContours[offBodyContourIndeces[i]])
+        offBodyCentroids.append(c)
+
+    # get the distance between the offbodies and the central centroid. Store in an array
+    distancesFromCentroid = []
+
+    for i in range(len(offBodyCentroids)):
+        print ("offpoint: " + str(offBodyCentroids[i]))
+        print("central point: " + str(mainBodyCentroid))
+        d = cvt.GetDistanceBetween(mainBodyCentroid, offBodyCentroids[i])
+        print(d)
+
+        # add to array
+        distancesFromCentroid.append(d)
+
+        # visualize the offbody connection to the central location
+        cvt.DrawOffBodyConnections(mainBodyCentroid, offBodyCentroids[i], origDraw)
+
+
+    # HACK get average dist from centroid
+    # HACK get max dist from centroid
 
 
 
-# Data to draw
-'''
-* trace cell contours
-* draw centroids
-'''
-
-# trace the cell contours
-# NOTE: dst3 is the last processed image
-trace = cvt.VisualizeFeatures(dst3, origDraw)
 
 
 
-'''
-////////////////////////////////////////////////////////////////////////////
-// Displaying Images
-////////////////////////////////////////////////////////////////////////////
-'''	
-# display images
-toDisplay = [orig, dst, dst1, dst2, dst3, trace]
-# TODO turn the other images into BGR so they don't look terrible\
-displayPanel.small_grid(toDisplay)
-# displayPanel.SingleView ("img", trace)
+    # Data to draw
+    '''
+    * trace cell contours
+    * draw centroids
+    '''
+
+    # trace the cell contours
+    # NOTE: dst3 is the last processed image
+    trace = cvt.VisualizeFeatures(dst3, origDraw)
 
 
-'''
-TODO:
-* Would like to have a slider to drag to adjust blur and see the effect across all the images
 
-* click on matplot img to open up a cv imshow?
-'''
+    '''
+    ////////////////////////////////////////////////////////////////////////////
+    // Displaying Images
+    ////////////////////////////////////////////////////////////////////////////
+    '''	
+    # display images
+    toDisplay = [orig, dst, dst1, dst2, dst3, trace]
+    # TODO turn the other images into BGR so they don't look terrible\
+    # displayPanel.small_grid(toDisplay)
+    displayPanel.SingleView ("img", trace)
+
+
+    '''
+    TODO:
+    * Would like to have a slider to drag to adjust blur and see the effect across all the images
+
+    * click on matplot img to open up a cv imshow?
+    '''
